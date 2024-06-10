f201fdbd479ca82ae37df0b2d8fd29b8f505d8f6
Guigzouz <benard.guillaume0@gmail.com>
2024-06-10 11:54:40 +0200

    feat:add dashboard component

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -1,10 +1,42 @@
+import React, { useState, useEffect } from "react";
 import HeaderComponent from "./HeaderComponent";
 
 export default function DashboardComponent() {
+  const [localStorageData, setLocalStorageData] = useState([]);
+
+  // Function to retrieve all data from local storage
+  useEffect(() => {
+    const dataFromLocalStorage = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      dataFromLocalStorage.push({ key, value });
+    }
+    setLocalStorageData(dataFromLocalStorage);
+  }, []);
+
   return (
     <>
       <HeaderComponent />
-      <div>here is my dashboard component</div>
+      <div>
+        <h2>Content of Local Storage</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Key</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {localStorageData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.key}</td>
+                <td>{item.value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
